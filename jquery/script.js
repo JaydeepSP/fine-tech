5d7ad5f0cb4f9a1440028a8a4be0dd3367191b9d
JaydeepSP <jaydeepprajapati456@gmail.com>
2025-07-28 10:40:15 +0530

    code uploaded

---
+++ jquery/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    // autoplay: true,
+    // centerMode: true,
+    infinite: true,
+    slidesToShow: 1,
+    speed: 700,
+    slidesToScroll: 1,
+    // adaptiveHeight: true,
+  });
+});
